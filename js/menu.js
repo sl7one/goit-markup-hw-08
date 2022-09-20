0e8af38de4bb4e759a0b4d6ebe6e443acf0507c6
Slava <110696118+sl7one@users.noreply.github.com>
2022-09-20 12:48:12 +0300

    .

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[.header-mob__button]'),
+    closeMenuBtn: document.querySelector('[.menu-button-close]'),
+    menu: document.querySelector('[.head-mob__modal]'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+  }
+})();
